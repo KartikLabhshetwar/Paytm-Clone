27eaf7462fa21a0dfbdcdb046fdf94785d10a306
Kartik Labhshetwar <kartik.labhshetwar@gmail.com>
2024-08-31 19:48:25 +0530

    Update index.js

--- paytm/backend/index.js
+++ paytm/backend/index.js
@@ -8,9 +8,10 @@ app.use(express.json())
 const mainRoute = require("./routes/index");
 app.use("/api/v1/", mainRoute);
 
+const port = process.env.PORT || 3000;
 
 
-app.listen(3000, ()=>{
+app.listen(port, ()=>{
     console.log("server is running");
     
 });
