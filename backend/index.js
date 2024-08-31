027af95879fe6146f55bfd91a66eac5e7937ec30
Kartik Labhshetwar <kartik.labhshetwar@gmail.com>
2024-08-31 20:50:40 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,20 @@
 const express = require("express");
-const cors = require("cors")
-const jwt = require("jsonwebtoken")
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const app = express();
+
+// Update CORS configuration
 app.use(cors({
-    origin: '*',
+    origin: 'https://paytm-clone-frontend.onrender.com', // Replace with your frontend URL
     credentials: true
-  }))
-app.use(express.json())
+}));
+
+app.use(express.json());
 
 const mainRoute = require("./routes/index");
 app.use("/api/v1/", mainRoute);
 
 const port = process.env.PORT || 3000;
-
-
-app.listen(port, ()=>{
-    console.log("server is running");
-    
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
-
-
