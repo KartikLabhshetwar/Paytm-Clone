5cb8b22946ce20eee23df2e201dd472d82075df4
Kartik Labhshetwar <kartik.labhshetwar@gmail.com>
2024-08-31 21:23:23 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const db = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config()
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+db.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Database connected successfully'))
   .catch((err) => {
     console.error('Database connection error:', err);
